41131e3ce70478367a666ea03c32782f2d36c6b9
TheJazzDev <babsman4all@gmail.com>
2024-11-30 05:45:07 +0100

    fix lint and prettier errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => (
-  <h1 className="text-white text-3xl font-black">
+  <h1 className="text-3xl font-black text-white">
     Welcome to the world of NextJS 15
   </h1>
-);
+)
 
-export default Home;
+export default Home
